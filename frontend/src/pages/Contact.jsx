bff93d6bf1abb7dd271de4c676bb13562f973aee
gishmaf global <gishmafglobalcompany@gmail.com>
2026-03-31 03:12:49 -0700

    removed helmet

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, useRef } from "react";
-import "../pages/Contact.css";
+import "./pages/Contact.css";
 import logo from "../assets/logo.png";
 
 export default function Contact() {
